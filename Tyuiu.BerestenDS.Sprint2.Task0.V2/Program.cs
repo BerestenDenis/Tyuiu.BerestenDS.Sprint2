27c2eb6104d0edbc4727322dbfa139fec314901a
Денис Берестень <d.blyl1337@gmail.com>
2024-10-01 22:29:11 +0500

    Add project files.

---
+++ Tyuiu.BerestenDS.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using Tyuiu.BerestenDS.Sprint2.Task0.V2.Lib;
+    class Program
+    {
+        static void Main(string[] args)
+    {
+            DataService ds = new DataService();
+        int x = 123;
+        int y = 123;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        Console.Title = "Спринт #2 | Выполнил: Берестень Д. С.  | ИБКСБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операции сравнения                                                *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Берестень Денис Сергеевич  | ИБКСБ 24-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*Написать программу из операций сравнений арифметических выражений,которая*");
+        Console.WriteLine("* вернет логическую последовательность(массив)                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+
+    }
+
+
+}
