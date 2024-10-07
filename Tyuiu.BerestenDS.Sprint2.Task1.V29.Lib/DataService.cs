0c64e2f6e47ea3cf0070ea49c669d114894f3867
Денис Берестень <d.blyl1337@gmail.com>
2024-10-07 12:44:37 +0500

    Task 1

---
+++ Tyuiu.BerestenDS.Sprint2.Task1.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.BerestenDS.Sprint2.Task1.V29.Lib
+{
+    public class DataService : ISprint2Task1V29
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c<d);
+            res[1] = (a < b+5) & (d<=c);
+            res[2] = (a < b + 6) || (d > c);
+            res[3] =(a < b) && (c > d);
+            res[4] = !res[3];
+            res[5] = (a > b) ^ (c<d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BerestenDS.Sprint2.Task1.V29/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using Tyuiu.BerestenDS.Sprint2.Task1.V29.Lib;
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int a = 657;
+        int b = 654;
+        int c = 657;
+        int d = 657;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+        Console.Title = "Спринт #2 | Выполнил: Берестень Д. С.  | ИБКСБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнил: Берестень Денис Сергеевич  | ИБКСБ 24-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений                                *");
+        Console.WriteLine("*также арифметических выражений,которая вернет логическ последовательность*");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(a = +a);
+        Console.WriteLine(b = +b);
+        Console.WriteLine(c = +c);
+        Console.WriteLine(d = +d);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+
+    }
