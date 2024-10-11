fdfcd2b678a655c766fbb389fad2c92b00fbe43b
Денис Берестень <d.blyl1337@gmail.com>
2024-10-11 23:42:03 +0500

    Task 4

---
+++ Tyuiu.BerestenDS.Sprint2.Task4.V14/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BerestenDS.Sprint2.Task4.V14.Lib;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Берестень Д. С.  | ИБКСБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнил: Берестень Денис Сергеевич  | ИБКСБ 24-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение переменной X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение переменной Y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x, y);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Значение функции = " + res);
+        Console.ReadKey();
+    }
+}
